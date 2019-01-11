542ff77ba073c139167f1980c82233f1785d89c3
Danny Mackey <dmack928@gmail.com>
2019-01-11 14:26:45 -0500

    nhl nfl

---
+++ pages/nfl.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="columns is-multiline">
+        <div
+          v-for="(article, index) in articles"
+          :key="index"
+          class="column is-one-quarter">
+          <a
+            :href="article.url"
+            target="_blank">
+            <div class="card">
+              <div class="card-image">
+                <figure class="image is-3by2">
+                  <img
+                    :src="article.urlToImage"
+                    :alt="article.title">
+                </figure>
+              </div>
+              <div class="card-content">
+                <div class="content">{{ article.title }}</div>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  async asyncData({ app }) {
+    const { articles } = await app.$axios.$get(
+      `https://newsapi.org/v2/top-headlines?sources=nfl-news&apiKey=${
+        process.env.API_KEY
+      }`
+    )
+
+    return { articles }
+  }
+}
+</script>
